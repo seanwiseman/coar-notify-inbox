b8ec26e91eba4bbf0ad8346712077cf8e7e83b55
Sean Wiseman <seanwiseman2012@gmail.com>
2023-08-09 14:06:04 +0100

    Add send notification webhook handler

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     mongo_db_uri: str = ""
+    on_receive_notification_webhook_url: str = ""
 
     model_config = SettingsConfigDict(env_file=".env")
 
---
+++ tasks/webhooks.py
@@ -0,0 +1,24 @@
+import json
+import logging
+import requests
+
+from db.models import Notification
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def send_notification_to_webhook(notification: Notification, webhook_url: str) -> None:
+    try:
+        response = requests.post(
+            url=webhook_url,
+            headers={"content-type": "application/ld+json"},
+            json=json.dumps(notification, default=str)
+        )
+
+        response.raise_for_status()
+
+        logger.info(f"Successfully sent notification to {webhook_url}. Status code: {response.status_code}")
+    except requests.RequestException:
+        logger.exception(f"Failed to send notification to {webhook_url}")
